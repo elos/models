6af9b575305d6d0cc889196f9516b57649f562d3
Nick Landolfi <nclandolfi@gmail.com>
2015-04-05 11:47:36 -0700

    more comments

--- abilities.go
+++ abilities.go
@@ -4,28 +4,66 @@ import (
 	"github.com/elos/data"
 )
 
+/*
+	Userable is the interface for a user-owned
+	model.
+*/
 type Userable interface {
 	SetUser(User) error
 	User(data.Access) (User, error)
 	UserID() data.ID
 }
 
+/*
+	ActionableOps is the interface for the operations
+	an Actionable should support.
+
+	It should be noted that ActionableOps and EventableOps
+	can be combined to make an Actionable and Eventable
+	model interface, without having the repeated data.Model
+	and Userable interfaces. An example is Fixture.
+*/
 type ActionableOps interface {
 	NextAction(data.Access) (Action, error)
 	StartAction(data.Access, Action) error
 	CompleteAction(data.Access, Action) error
 }
 
+/*
+	EventableOps is the interface for the operations
+	an Eventable should support.
+
+	It should be noted that EventableOps and ActionableOps
+	can be combined to make an Eventable and Actionable
+	model interface, without having the repeated data.Model
+	and Userable interfaces. An example is Fixture.
+*/
 type EventableOps interface {
 	NextEvent(data.Access) (Event, error)
 }
 
+/*
+	Actionable is the interface for a model which can
+	be treated like an actionable.
+
+	The data.Model requirements are used to extract the
+	data.Kind and data.ID from the model which fulfills
+	this interface.
+*/
 type Actionable interface {
 	data.Model
 	Userable
 	ActionableOps
 }
 
+/*
+	Eventable is the interface for a model which can
+	be treated like an eventable.
+
+	The data.Model requirements are used to extract the
+	data.Kind and data.ID from the model which fulfills
+	this interface
+*/
 type Eventable interface {
 	data.Model
 	Userable
