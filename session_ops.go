63e7a071c3e16abbe180dee1c9abe2f798dc2b48
Nick Landolfi <nclandolfi@gmail.com>
2016-06-12 11:35:27 -0700

    add session ops

--- session_ops.go
+++ session_ops.go
@@ -1,14 +1,15 @@
 package models
 
 import (
-	"log"
 	"time"
 
 	"github.com/elos/data"
 )
 
+// TODO(nclandolfi): need to ensure the generated token is unique, can take the risk for now though
 func NewSessionForUser(u *User) *Session {
-	log.Print("don't use") // <-- lol, what?
+	//log.Print("don't use") // <-- lol, what? (might be because session has a credential, so that if a credential is cancelled we can cancel the session. This is not currently implemented)
+
 	s := &Session{
 		CreatedAt:    time.Now(),
 		UpdatedAt:    time.Now(),
@@ -21,10 +22,6 @@ func NewSessionForUser(u *User) *Session {
 	return s
 }
 
-func (s *Session) Valid() bool {
-	return time.Now().Sub(s.CreatedAt.Add(time.Duration(s.ExpiresAfter)*time.Second)) < 0
-}
-
 func SessionForToken(db data.DB, token string) (*Session, error) {
 	session := NewSession()
 	if err := db.PopulateByField("token", token, session); err != nil {
@@ -32,4 +29,12 @@ func SessionForToken(db data.DB, token string) (*Session, error) {
 	}
 
 	return session, nil
+}
+
+func (s *Session) Valid() bool {
+	return time.Now().Sub(s.CreatedAt.Add(time.Duration(s.ExpiresAfter)*time.Second)) < 0
+}
+
+func (s *Session) Expires() time.Time {
+	return s.CreatedAt.Add(time.Duration(s.ExpiresAfter) * time.Second)
 }
